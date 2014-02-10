f606d6ba02229fdd7a791294f32a760d74a5cb01
Jesse Warren <jesse@jesse.warren>
2014-02-10 01:59:16 -0800

    divided into grids

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-$(function() {
-    function allowDrop(ev) {
-        ev.preventDefault();
-    }
-
-    function drag(ev) {
-        ev.dataTransfer.setData("Text", ev.target.id);
-    }
-
-    function drop(ev) {
-        ev.preventDefault();
-        var data = ev.dataTransfer.getData("Text");
-        ev.target.appendChild(document.getElementById(data));
-    }
-});
-
-
+var a;
